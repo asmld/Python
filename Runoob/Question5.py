e50d0c12b0c2629050c1ac4c6cb831a8cb2d5c1b
asmld <1361673198@qq.com>
2021-04-18 16:30:31 +0800

    New File

--- Runoob/Question5.py
+++ Runoob/Question5.py
@@ -2,6 +2,6 @@ n = int(input())  # 回顾：input函数返回的永远是字符串
 num = []
 for i in range(n):
     num.append(int(input('请输入第%d个数：' % (i + 1))))  # 思考：此循环如何实现把输入的n个数放进列表的？
-num.sort(reverse=True)
+num.sort(reverse=True)  # 思考：如果使用sorted函数该如何写？
 for i in num:
     print(i, end=' ')  # 回顾：print函数的几个参数
