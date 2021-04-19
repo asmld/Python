03731a173a9722fdf489d835d3be02d3b851c0ff
asmld <1361673198@qq.com>
2021-04-19 22:41:48 +0800

    New File

--- HomeWork/上机作业2/Dataset.py
+++ HomeWork/上机作业2/Dataset.py
@@ -3,6 +3,6 @@ f = pd.ExcelFile('./Dataset_city.xlsx')
 data = pd.DataFrame(columns=['标题', '摘要', '网址'])
 for i in f.sheet_names:
     data1 = pd.read_excel('./Dataset_city.xlsx', sheet_name=i)
-    data1 = data1.iloc[:,[13, 14, 15]]
+    data1 = data1.iloc[:, [13, 14, 15]]
     data = pd.concat([data, data1], axis=0, ignore_index=True)
 data.to_excel('result.xlsx')
--- HomeWork/上机作业2/NCV.py
+++ HomeWork/上机作业2/NCV.py
@@ -3,7 +3,7 @@ data1 = pd.read_excel('./NCV.xls', sheet_name='Sheet1')
 df = pd.DataFrame(columns=['时间', '确诊', '日增', '死亡', '国家'])
 col = ['时间', '确诊', '日增', '死亡', '国家']
 for i in range(25):
-    data = data1.iloc[1:,[0, 3*i+1, 3*i+2, 3*i+3]]
+    data = data1.iloc[1:, [0, 3*i+1, 3*i+2, 3*i+3]]
     country = data.columns[1]
     data['国家'] = country
     dic = {data.columns[x]: col[x] for x in range(5)}
